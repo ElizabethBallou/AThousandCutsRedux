c714a3b692ec7785b9d658f08b14db232483f247
rowanq <rowanqueue@gmail.com>
2020-02-17 19:15:49 -0500

    text notifications on the main screen

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -8,6 +8,9 @@ public class GameController : MonoBehaviour
     public GameObject rosaPrefab;
     public Transform choicesParent;
     public TextAsset inkJsonAsset;
+    public GameObject mainPageParent;
+    public bool isTexting;
+    public bool textWaiting;
     // Start is called before the first frame update
     void Awake()
     {
@@ -17,14 +20,29 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Services.InkManager.Update();
+        if(isTexting){
+            textWaiting = false;
+            Services.InkManager.Update();
+        }else{
+            if(textWaiting == false){
+                Services.InkManager.Update();
+                string talkingTo = Services.InkManager.currentConversant;
+                Services.CharacterManager.characters[talkingTo].textNotification.text = talkingTo+"(1)";
+                textWaiting = true;
+            }
+        }
+        if(Input.GetKeyDown(KeyCode.Backspace)){
+            foreach(string character in Services.CharacterManager.characters.Keys){
+                Services.CharacterManager.characters[character].transform.gameObject.SetActive(false);
+            }
+        }
     }
 
     public void InitializeServices(){
         Services.GameController = this;
 
         Services.CharacterManager = new NewCharacterManager();
-        Services.CharacterManager.Initialize();
+        Services.CharacterManager.Initialize(mainPageParent.transform);
 
         Services.InkManager = new InkManager(inkJsonAsset);
 
@@ -33,5 +51,11 @@ public class GameController : MonoBehaviour
      public void SelectChoice(int choice){
         Debug.Log(choice);
         Services.InkManager.SelectChoice(choice);
+    }
+    public void SelectCharacter(string character){
+        Debug.Log(character);
+        Services.CharacterManager.characters[character].transform.gameObject.SetActive(true);
+        Services.CharacterManager.characters[character].textNotification.text = character;
+        isTexting = true;
     }
 }
--- Assets/Scripts/NewCharacterManager.cs
+++ Assets/Scripts/NewCharacterManager.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
+public enum CharacterState{
+    NoText,
+    OnRead,
+    TalkingTo
+}
 public class NewCharacterManager
 {
     public Dictionary<string, Character> characters = new Dictionary<string, Character>();
@@ -11,28 +16,28 @@ public class NewCharacterManager
     private Image[] ConversantPictures;
 
     // Start is called before the first frame update
-    public void Initialize()
+    public void Initialize(Transform mainPageParent)
     {
         characters.Add("Olivia",new Character("Olivia",GameObject.Find("Olivia").transform));
+        characters["Olivia"].textNotification = mainPageParent.GetChild(0).GetComponent<TextMeshProUGUI>();
         characters.Add("Mikaela",new Character("Mikaela",GameObject.Find("Mikaela").transform));
+        characters["Mikaela"].textNotification = mainPageParent.GetChild(1).GetComponent<TextMeshProUGUI>();
         characters.Add("Duane", new Character("Duane",GameObject.Find("Duane").transform));
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
+        characters["Duane"].textNotification = mainPageParent.GetChild(2).GetComponent<TextMeshProUGUI>();
     }
 }
 public class Character
 {
     public string name;
+    public CharacterState state = CharacterState.NoText;
     public Transform transform;
     public float height = 0;
     public List<string> texts = new List<string>();
+    public TextMeshProUGUI textNotification;
     public Character(string name, Transform place){
         this.name = name;
         this.transform = place;
+        transform.gameObject.SetActive(false);
     }
     
     
